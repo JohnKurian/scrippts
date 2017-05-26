2d0499ddb217b1df949e0a7be93829971a7981b1
John Joy <johnjoykurian@gmail.com>
2017-05-26 15:20:54 +0530

    added firebase hosting

--- script.js
+++ script.js
@@ -5,16 +5,18 @@
 var firebase = require("firebase");
 
 var config = {
-    apiKey: " AIzaSyD-YSGgW0aQIiiaeFuKCdTTONi-ViN5xzQ",
-    authDomain: "argument-staging.firebaseapp.com",
-    databaseURL: "https://argument-staging.firebaseio.com",
-    storageBucket: "argument-staging.appspot.com",
+    apiKey: "AIzaSyAg-UlKP9oonj4IMo5op6qEAOzFXHZloiY",
+    authDomain: "argument-app.firebaseapp.com",
+    databaseURL: "https://argument-app.firebaseio.com",
+    projectId: "argument-app",
+    storageBucket: "argument-app.appspot.com",
+    messagingSenderId: "626987124454"
 };
+
 firebase.initializeApp(config);
 
 
 
-var database = firebase.database();
 
 tree = {0: {data: "parent", children: [{data: "child1", children: {0: {data: "grandchild2"}}}, {data: "child2", children: {0: {data: "grandchild2"}}}]}};
 
@@ -23,11 +25,10 @@ tree = {0: {data: "parent", children: [{data: "child1", children: {0: {data: "gr
 
 
 
-var list = [];
 
 
 
-// ref.set({'check': 'this'})
+firebase.database().ref().child('test').set(tree)
 
-firebase.database().ref().child('test').once('value', function(snap) { console.log(snap.val()) });
+// firebase.database().ref().child('test').once('value', function(snap) { console.log(snap.val()) });
 
