8c3feda7f609e340ca4e075a30cd8171233fd57a
John Joy <johnjoykurian@gmail.com>
2017-11-22 02:34:41 +0530

    * fix modal overlay z-index bug * update to react 16

--- src/App.js
+++ src/App.js
@@ -34,6 +34,15 @@ const customStyles = {
         bottom                : 'auto',
         marginRight           : '-50%',
         transform             : 'translate(-50%, -50%)'
+    },
+    overlay: {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: "rgba(255, 255, 255, 0.75)",
+        zIndex: 10000
     }
 };
 
