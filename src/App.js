426974c53958f6897aef723d8bcd0ae4daa29c1b
John Joy <johnjoykurian@gmail.com>
2018-01-19 01:03:11 +0530

    fix safari user deletion bug

--- src/App.js
+++ src/App.js
@@ -897,25 +897,37 @@ class Profile extends Component {
 
             db.collection("users").doc(this.props.user.uid).collection('scripts')
                 .where('creator', '==', true).get().then(function(querySnapshot) {
-                console.log('begin script deletion');
                 let counter = 0;
+
+                if(querySnapshot.size===0) {
+
+                    db.collection("users").doc(this.props.user.uid).delete().then(function() {
+
+                        user.delete().then(function() {
+                            this.props.history.push('/');
+                            window.location.reload();
+                        }.bind(this)).catch(function(error) {
+                            // An error happened.
+                        });
+
+                    }.bind(this)).catch(function(error) {
+                        console.error("Error removing user document: ", error);
+                    });
+                }
+
                 querySnapshot.forEach(function(script) {
                     counter++;
 
                     db.collection("scripts").doc(script.id).delete().then(function() {
-                        console.log("script successfully deleted!");
                     }.bind(this)).catch(function(error) {
                         console.error("Error removing document: ", error);
                     }.bind(this));
 
                     if(counter===querySnapshot.size) {
-                        console.log('end deletion..begin user deletion');
 
                         db.collection("users").doc(this.props.user.uid).delete().then(function() {
-                            console.log("user document deleted!");
 
                             user.delete().then(function() {
-                                console.log('user deleted successfully.');
                                 this.props.history.push('/');
                                 window.location.reload();
                             }.bind(this)).catch(function(error) {
